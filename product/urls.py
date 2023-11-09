5615f388ef002f5151262c27b1296cb39e503898
sinonshino <12pfieldb@gmail.com>
2023-11-09 22:48:56 +0100

    Added in another template for individual product pages and also introduced template block code which can be reused

--- product/urls.py
+++ product/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),      # This is from a view called product_list!
+    path('product/<int:pk>/', views.product_detail, name='product_detail'),
 
 ]
--- product/views.py
+++ product/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product     # Can use .models as it is in the same directory / application as this file
 
 # Create your views here.
 
 def product_list(request):
     products = Product.objects.all().order_by('-date_added')     # Returns all objects with Product and orders them by date added, with the oldest results showing first
-    return render(request, 'product/product_list.html', {'products': products})
+    return render(request, 'product/product_list.html', {'products': products})     # product/product_list.html is the template that displays
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'product/product_detail.html', {'product': product})     # product/product_detail.html is the template that displays
