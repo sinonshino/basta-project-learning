afa9a53cab1a48343d88ca963e70bcdb71073fb3
sinonshino <12pfieldb@gmail.com>
2023-11-09 21:25:38 +0100

    Modified the template and views.py to add dynamic data to template, and used Queryset in view

--- product/views.py
+++ product/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Product     # Can use .models as it is in the same directory / application as this file
 
 # Create your views here.
 
 def product_list(request):
-    return render(request, 'product/product_list.html', {})
+    products = Product.objects.all().order_by('-date_added')     # Returns all objects with Product and orders them by date added, with the oldest results showing first
+    return render(request, 'product/product_list.html', {'products': products})
